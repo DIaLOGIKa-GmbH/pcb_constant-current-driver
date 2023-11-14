0e1eaa5fe904c64060a92633d23c6eb7ea31696e
Tim Goll <tim.goll@dialogika.de>
2023-11-14 19:15:36 +0100

    added LM3404 driver

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# AL8860 Constant Current Driver
+# LED Constant Current Driver Design
+
+This project is part of [LibroDuct](https://www.libroduct.com/). We decided to publish some results of our LED driver designs to support the opensource maker scene. Feel free to use those results in your designs.
+
+## LM3404 Constant Current Driver
+
+## AL8860 Constant Current Driver
 
--- benchmarks/plot_data.py
+++ benchmarks/plot_data.py
@@ -3,19 +3,59 @@ import plt
 
 folder = str(pathlib.Path(__file__).parent.resolve())
 
-### IR LED driver
-data = pandas.read_csv(folder + "/data/measurement.csv", comment='#')
+### LED driver
+data_lm3404 = pandas.read_csv(folder + "/data/measurement_lm3404.csv", comment='#')
+data_al8860 = pandas.read_csv(folder + "/data/measurement_al8860.csv", comment='#')
+
+### LM3404 ###
 
 plt.plt(
     [
-        data["V_in"],
-        data["V_in"],
-        data["V_in"]
+        data_lm3404["V_in"],
+        data_lm3404["V_in"]
     ],
     [
-        data["I_in"],
-        data["I_out"],
-        data["I_des"]
+        data_lm3404["I_in"],
+        data_lm3404["I_out"]
+    ],
+    [
+        "Input Current",
+        "Output Current"
+    ],
+    "V_in [V]",
+    "I [mA]",
+    (0, 650),
+    folder + "/plots/measurement_lm3404.png"
+)
+
+plt.plt(
+    [
+        data_lm3404["V_in"]
+    ],
+    [
+        (data_lm3404["I_out"] * data_lm3404["V_out"]) / (data_lm3404["I_in"] * data_lm3404["V_in"])
+    ],
+    [
+        ""
+    ],
+    "V_in [V]",
+    "efficiency",
+    (0.8, 1),
+    folder + "/plots/measurement_lm3404_eff.png"
+)
+
+### AL8860 ###
+
+plt.plt(
+    [
+        data_al8860["V_in"],
+        data_al8860["V_in"],
+        data_al8860["V_in"]
+    ],
+    [
+        data_al8860["I_in"],
+        data_al8860["I_out"],
+        data_al8860["I_des"]
     ],
     [
         "Input Current",
@@ -25,15 +65,15 @@ plt.plt(
     "V_in [V]",
     "I [mA]",
     (0, 800),
-    folder + "/plots/measurement.png"
+    folder + "/plots/measurement_al8860.png"
 )
 
 plt.plt(
     [
-        data["V_in"]
+        data_al8860["V_in"]
     ],
     [
-        (data["I_out"] * data["V_out"]) / (data["I_in"] * data["V_in"])
+        (data_al8860["I_out"] * data_al8860["V_out"]) / (data_al8860["I_in"] * data_al8860["V_in"])
     ],
     [
         ""
@@ -41,5 +81,5 @@ plt.plt(
     "V_in [V]",
     "efficiency",
     (0.8, 1),
-    folder + "/plots/measurement_eff.png"
+    folder + "/plots/measurement_al8860_eff.png"
 )
